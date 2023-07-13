846442dcd04a74cdd07920ba315cbbc158669f14
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-07-13 16:25:08 +0100

    resolving build error

--- src/components/utils/helper.ts
+++ src/components/utils/helper.ts
@@ -1,4 +1,7 @@
 export const decodeEntities = (html: string): string => {
+	if (typeof document === 'undefined') {
+		return html;
+	}
 	const txt = document.createElement('textarea');
 	txt.innerHTML = html;
 	return txt.value;
